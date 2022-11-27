0be9972f68260131b6c5c916357dd255a445d4db
root <williamf1245@gmail.com>
2022-11-27 15:15:04 +0000

    first commit

---
+++ moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'Hik9z6YE7IzFcQk50GnV1MAH3rynSB6N206.189.27.212';
+$CFG->bootstraphash = '428e6794ec261e12ce3d7ec380d1e6ca';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
